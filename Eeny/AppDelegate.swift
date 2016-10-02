15c1184f8e7084971e0c0d3c70961d88e0a03793
cavra <coryavra@gmail.com>
2016-10-02 05:33:32 -0500

    Ugh

--- Eeny/AppDelegate.swift
+++ Eeny/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GooglePlaces
+import GooglePlacePicker
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSPlacesClient.provideAPIKey("AIzaSyBKG7IgahOPmjJXPa4MYFb7YHnTf_26Zf0")
+
         return true
     }
 
--- Eeny/RestaurantsViewController.swift
+++ Eeny/RestaurantsViewController.swift
@@ -7,22 +7,49 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class RestaurantsViewController: UIViewController {
     
     // Outlets
-    @IBOutlet weak var restaurantsCollectionView: UICollectionView!
+    @IBOutlet weak var restLabel: UILabel!
+   
+    // Variables
+    var places: [NSDictionary]?
+    var placesClient: GMSPlacesClient?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        placesClient = GMSPlacesClient.shared()
 
-        // Do any additional setup after loading the view.
+        // Get the data from the network
+        loadDataFromNetwork()
     }
-
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func loadDataFromNetwork() {
+        
+        placesClient?.currentPlace(callback: {
+            (placeLikelihoodList: GMSPlaceLikelihoodList?, error: Error?) -> Void in
+            if let error = error {
+                    print("Pick Place error: \(error.localizedDescription)")
+                return
+        }
+
+        self.restLabel.text = "No current place"
+
+        if let placeLikelihoodList = placeLikelihoodList {
+            let place = placeLikelihoodList.likelihoods.first?.place
+                if place != nil {
+                    self.restLabel.text = place?.name
+                    //self.addressLabel.text = place.formattedAddress!.componentsSeparatedByString(", ").joinWithSeparator("\n")
+                }
+            }
+        })
+    }
 
 }
